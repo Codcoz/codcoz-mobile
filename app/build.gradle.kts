3d515e782bbbc94404f279d137eb03127e3e20f9
laurawada <laura.wada@germinare.org.br>
2025-09-22 10:19:36 -0300

    feat: adicionando as configurações de cor nos dias que têm tarefas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.legacy.support.v4)
     implementation(libs.recyclerview)
+    implementation(libs.lifecycle.viewmodel)
+    // The view calendar library for Android
+    implementation("com.kizitonwose.calendar:view:2.8.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
