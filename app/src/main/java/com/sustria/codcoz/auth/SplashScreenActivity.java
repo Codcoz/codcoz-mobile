99d68645e0a2f7afc21ffde4c7d547c396ef236a
Filipi Inacio Santos <filipiinacio25251120@gmail.com>
2025-09-30 11:53:39 -0300

    merge: Mergeando a auth com essa branch e resolvendo erros

--- app/src/main/java/com/sustria/codcoz/auth/SplashScreenActivity.java
+++ app/src/main/java/com/sustria/codcoz/auth/SplashScreenActivity.java
@@ -22,7 +22,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash_screen);
 
         ImageView logo = findViewById(R.id.imageViewLogo);
 
