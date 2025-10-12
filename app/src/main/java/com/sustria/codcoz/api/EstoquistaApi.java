10574dd7c4011252754ce1ddddd7a3ab26faa46f
Filipi Inacio Santos <filipiinacio25251120@gmail.com>
2025-10-12 16:09:32 -0300

    fix: Modificando o formato que vem da url
    Formato da url foi modificado e foi necessario mudar na api estoque

--- app/src/main/java/com/sustria/codcoz/api/EstoquistaApi.java
+++ app/src/main/java/com/sustria/codcoz/api/EstoquistaApi.java
@@ -4,13 +4,11 @@ import com.sustria.codcoz.api.model.EstoquistaResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface EstoquistaApi {
 
-    @GET("estoquista/buscar")
-    Call<EstoquistaResponse> buscarPorEmail(@Query("email") String email);
+    @GET("estoquista/buscar/{email}")
+    Call<EstoquistaResponse> buscarPorEmail(@Path("email") String email);
 }
-
-
-
